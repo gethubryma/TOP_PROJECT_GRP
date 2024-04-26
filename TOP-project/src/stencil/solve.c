e1238cf670cf4e9ad62bde6b4f70f572efd67d0c
Arhab Sofiane <arhabsofiane123@gmail.com>
2024-04-26 11:07:33 +0200

    Added fast exponentiation algo, instead of pow()

--- TOP-project/src/stencil/solve.c
+++ TOP-project/src/stencil/solve.c
@@ -11,6 +11,28 @@
 #define min(a, b) ((a) < (b) ? (a) : (b))
 
 
+double power_of_17(usz exponent) {
+    if (exponent == 0) return 1.0;
+    if (exponent == 1) return 17.0;
+
+    double result = 1.0;
+    double base = 17.0;
+
+    while (exponent > 0) {
+        if (exponent % 2 == 1) {
+            result *= base;
+        }
+        base *= base;  // Square the base
+        exponent /= 2; // Divide the exponent by 2
+    }
+
+    return result;
+}
+
+
+
+
+/*
 void solve_jacobi(mesh_t* A, mesh_t const* B, mesh_t* C) {
     assert(A->dim_x == B->dim_x && B->dim_x == C->dim_x);
     assert(A->dim_y == B->dim_y && B->dim_y == C->dim_y);
@@ -52,6 +74,55 @@ void solve_jacobi(mesh_t* A, mesh_t const* B, mesh_t* C) {
     }
     mesh_copy_core(A, C);
 }
+
+
+
+*/
+
+void solve_jacobi(mesh_t* A, mesh_t const* B, mesh_t* C) {
+    assert(A->dim_x == B->dim_x && B->dim_x == C->dim_x);
+    assert(A->dim_y == B->dim_y && B->dim_y == C->dim_y);
+    assert(A->dim_z == B->dim_z && B->dim_z == C->dim_z);
+
+    usz const dim_x = A->dim_x;
+    usz const dim_y = A->dim_y;
+    usz const dim_z = A->dim_z;
+
+    for(usz kk = STENCIL_ORDER; kk < dim_z - STENCIL_ORDER ; kk+=BLOCK_SIZE_Z ){
+        for(usz jj = STENCIL_ORDER; jj < dim_y - STENCIL_ORDER ; jj += BLOCK_SIZE_Y){
+            for(usz ii = STENCIL_ORDER; ii < dim_x - STENCIL_ORDER ; ii += BLOCK_SIZE_X){
+
+                for (usz k = kk; k < min(dim_z - STENCIL_ORDER, kk + BLOCK_SIZE_Z); ++k) {
+                    for (usz j =  jj ; j < min(dim_y - STENCIL_ORDER,jj + BLOCK_SIZE_Y); ++j) {
+                        for (usz i = ii ; i < min(dim_x - STENCIL_ORDER, ii + BLOCK_SIZE_X); ++i) {
+                            usz idx = i * dim_y * dim_z + j * dim_z + k;
+                            C->cells.value[idx] = A->cells.value[idx] * B->cells.value[idx];
+
+                            for (usz o = 1; o <= STENCIL_ORDER; ++o) {
+                                C->cells.value[idx] += A->cells.value[(i + o) * dim_y * dim_z + j * dim_z + k] *
+                                                        B->cells.value[(i + o) * dim_y * dim_z + j * dim_z + k] / power_of_17(o);
+                                C->cells.value[idx] += A->cells.value[(i - o) * dim_y * dim_z + j * dim_z + k] *
+                                                        B->cells.value[(i - o) * dim_y * dim_z + j * dim_z + k] /  power_of_17(o);
+                                C->cells.value[idx] += A->cells.value[i * dim_y * dim_z + (j + o) * dim_z + k] *
+                                                        B->cells.value[i * dim_y * dim_z + (j + o) * dim_z + k] /  power_of_17(o);
+                                C->cells.value[idx] += A->cells.value[i * dim_y * dim_z + (j - o) * dim_z + k] *
+                                                        B->cells.value[i * dim_y * dim_z + (j - o) * dim_z + k] /  power_of_17(o);
+                                C->cells.value[idx] += A->cells.value[i * dim_y * dim_z + j * dim_z + (k + o)] *
+                                                        B->cells.value[i * dim_y * dim_z + j * dim_z + (k + o)] /  power_of_17(o);
+                                C->cells.value[idx] += A->cells.value[i * dim_y * dim_z + j * dim_z + (k - o)] *
+                                                        B->cells.value[i * dim_y * dim_z + j * dim_z + (k - o)] /  power_of_17(o);
+                            }
+                        }
+                    } 
+                }
+            }    
+        }        
+    }
+    mesh_copy_core(A, C);
+}
+*/
+
+
 
 
 
