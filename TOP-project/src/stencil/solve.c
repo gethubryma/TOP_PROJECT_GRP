2361b06d57223d46249cb6c8041b144faca04fdd
Arhab Sofiane <arhabsofiane123@gmail.com>
2024-05-03 17:28:06 +0200

    Optimised solve_jacobi to favorise vectorization

--- TOP-project/src/stencil/solve.c
+++ TOP-project/src/stencil/solve.c
@@ -2,6 +2,7 @@
 
 #include <assert.h>
 #include <math.h>
+#include <omp.h>
 
 
 #define BLOCK_SIZE_X 64
@@ -30,7 +31,6 @@ double power_of_17(usz exponent) {
 }
 
 
-
 void solve_jacobi(mesh_t* A, mesh_t const* B, mesh_t* C) {
     assert(A->dim_x == B->dim_x && B->dim_x == C->dim_x);
     assert(A->dim_y == B->dim_y && B->dim_y == C->dim_y);
@@ -40,45 +40,51 @@ void solve_jacobi(mesh_t* A, mesh_t const* B, mesh_t* C) {
     usz const dim_y = A->dim_y;
     usz const dim_z = A->dim_z;
 
-    
+
+
+    // Precompute
     f64 powers[STENCIL_ORDER];
-    #pragma omp parallel for schedule(static) 
     for (usz o = 1; o <= STENCIL_ORDER; ++o) {
-        #pragma omp critical
-        powers[o-1] = 1.0 / power_of_17((f64)o);
+        powers[o-1] = 1.0 / power_of_17(o);
     }
+    usz dim_yz = dim_y * dim_z;
+    usz dim_z_minus_ST = dim_z - STENCIL_ORDER;
+    usz dim_x_minus_ST = dim_x - STENCIL_ORDER;
+    usz dim_y_minus_ST = dim_y - STENCIL_ORDER;
 
-    #pragma omp parallel for collapse(3) schedule(static)
-    for(usz kk = STENCIL_ORDER; kk < dim_z - STENCIL_ORDER ; kk+=BLOCK_SIZE_Z ){
-        for(usz jj = STENCIL_ORDER; jj < dim_y - STENCIL_ORDER ; jj += BLOCK_SIZE_Y){
-            for(usz ii = STENCIL_ORDER; ii < dim_x - STENCIL_ORDER ; ii += BLOCK_SIZE_X){
 
-                for (usz i = ii; i < min(dim_x - STENCIL_ORDER, ii + BLOCK_SIZE_X); ++i) {
-                    for (usz j =  jj ; j < min(dim_y - STENCIL_ORDER,jj + BLOCK_SIZE_Y); ++j) {
-                        for (usz k = kk ; k < min(dim_z - STENCIL_ORDER, kk + BLOCK_SIZE_Z); ++k) {
-                            usz idx = i * dim_y * dim_z + j * dim_z + k;
-                            C->cells.value[idx] = A->cells.value[idx] * B->cells.value[idx];
+    for (usz kk = STENCIL_ORDER; kk < dim_z_minus_ST; kk += BLOCK_SIZE_Z) {
+        for (usz jj = STENCIL_ORDER; jj < dim_y_minus_ST; jj += BLOCK_SIZE_Y) {
+            for (usz ii = STENCIL_ORDER; ii < dim_x_minus_ST; ii += BLOCK_SIZE_X) {
+
+                for (usz i = ii; i < min(dim_x_minus_ST, ii + BLOCK_SIZE_X); ++i) {
+                    for (usz j = jj; j < min(dim_y_minus_ST, jj + BLOCK_SIZE_Y); ++j) {
+                        for (usz k = kk; k < min(dim_z_minus_ST, kk + BLOCK_SIZE_Z); ++k) {
+                            usz idx = i * dim_yz + j * dim_z + k;
+                            f64 sum = A->cells.value[idx] * B->cells.value[idx];
 
                             for (usz o = 1; o <= STENCIL_ORDER; ++o) {
-                                C->cells.value[idx] += A->cells.value[(i + o) * dim_y * dim_z + j * dim_z + k] *
-                                                        B->cells.value[(i + o) * dim_y * dim_z + j * dim_z + k] * powers[o-1];
-                                C->cells.value[idx] += A->cells.value[(i - o) * dim_y * dim_z + j * dim_z + k] *
-                                                        B->cells.value[(i - o) * dim_y * dim_z + j * dim_z + k] * powers[o-1];
-                                C->cells.value[idx] += A->cells.value[i * dim_y * dim_z + (j + o) * dim_z + k] *
-                                                        B->cells.value[i * dim_y * dim_z + (j + o) * dim_z + k] * powers[o-1];
-                                C->cells.value[idx] += A->cells.value[i * dim_y * dim_z + (j - o) * dim_z + k] *
-                                                        B->cells.value[i * dim_y * dim_z + (j - o) * dim_z + k] * powers[o-1];
-                                C->cells.value[idx] += A->cells.value[i * dim_y * dim_z + j * dim_z + (k + o)] *
-                                                        B->cells.value[i * dim_y * dim_z + j * dim_z + (k + o)] * powers[o-1];
-                                C->cells.value[idx] += A->cells.value[i * dim_y * dim_z + j * dim_z + (k - o)] *
-                                                        B->cells.value[i * dim_y * dim_z + j * dim_z + (k - o)] * powers[o-1];
+                                usz idx_plus_o = (i + o) * dim_yz + j * dim_z + k;
+                                usz idx_minus_o = (i - o) * dim_yz + j * dim_z + k;
+                                sum += (A->cells.value[idx_plus_o] * B->cells.value[idx_plus_o] +
+                                        A->cells.value[idx_minus_o] * B->cells.value[idx_minus_o]) * powers[o - 1];
+
+                                usz idx_plus_o_dimz = i * dim_yz + (j + o) * dim_z + k;
+                                usz idx_minus_o_dimz = i * dim_yz + (j - o) * dim_z + k;
+                                sum += (A->cells.value[idx_plus_o_dimz] * B->cells.value[idx_plus_o_dimz] +
+                                        A->cells.value[idx_minus_o_dimz] * B->cells.value[idx_minus_o_dimz]) * powers[o - 1];
+
+                                usz idx_plus_o_dimyz = i * dim_yz + j * dim_z + (k + o);
+                                usz idx_minus_o_dimyz = i * dim_yz + j * dim_z + (k - o);
+                                sum += (A->cells.value[idx_plus_o_dimyz] * B->cells.value[idx_plus_o_dimyz] +
+                                        A->cells.value[idx_minus_o_dimyz] * B->cells.value[idx_minus_o_dimyz]) * powers[o - 1];
                             }
+                            C->cells.value[idx] = sum;
                         }
-                    } 
+                    }
                 }
-            }    
-        }        
+            }
+        }
     }
     mesh_copy_core(A, C);
 }
-
